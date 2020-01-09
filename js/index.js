a8060e48f5f695df16a88a900e83afca4b9dcbd5
lss <361490334@qq.com>
2020-01-09 11:25:50 +0800

    zy1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// $(function(){
+// 	$(".img").mouseover(function(){
+// 		$(this).css({
+// 			"cursor": 'pointer',
+// 			"border": '1px solid #158FE5'
+// 		});
+// 	});
+// 	$(".intro").mouseover(function(){
+// 		$(this).css({
+// 			"cursor": 'pointer',
+// 			"background-color": '#158FE5',
+// 			"color": '#fff'
+// 		});
+// 	});
+// 	$(".prod").mouseenter(function(){
+// 		$(".prod .intro,.prod.img").trigger("mouseenter");
+// 	})
+// })
